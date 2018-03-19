e468dd2eb2d5310b763045ebbcc21aae16c3a108
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-19 16:42:42 +0530

    Create 1121.c

---
+++ 1121.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)#include <stdio.h>
+int main(#include <stdio.h>
+int main(void) {
+char str[20];
+int n;
+scanf("%s %d",str,n);
+for(int i=0;i<3;i++)
+{
+	printf("%c",str[i]);
+}
+return 0;
+}
+int x,y;
+pri
