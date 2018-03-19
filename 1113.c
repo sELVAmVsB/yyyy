3b20260b3e4ffb8902968a63004b8d63b914614a
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-19 16:20:18 +0530

    Create 1113.c

---
+++ 1113.c
@@ -0,0 +1,26 @@
+//print the number given by the user by spliting
+#include <stdio.h>#53 problem
+l=list(input("Enter Number"))
+print(sum(map(int,l)))
+int man=n/10;
+        }
+     while(rev!=0)
+    {
+       dig=rev%10;
+        rev=rev/10;
+        printf("%d ",dig);
+    }
+       
+}
+}
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("ENTER THE INPUT NUMBER\n");
+	scanf("%d",&n);
+	if((n%2)==0)
+	printf("THE NEAREST EVEN NUMBER IS %d",n);
+	else
+	printf("THE NEAREST EVEN NUMBER IS %d",n-1);
+}
