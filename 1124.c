1f58eaa773854c550c25d016060a6ebb76e5988b
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-19 16:47:45 +0530

    Create 1124.c

---
+++ 1124.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+i#include<stdio.h>
+#include<g namespace std;
+
+int main() {
+	int m,n,c;
+	cin>>m>>n;
+	c=m+n;
+	if(c%2==0)
+	{
+	    cout<<"even";
+	}
+	else
+	{
+	    cout<<"odd";
+	}
+	}
+    printf("%d",count+1);
+	return 0;
+}
+else
+{
+printf("No");
+}
+}
