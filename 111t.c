43c097e213a112dda8dc4bf66b01b589e2b262cc
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-19 16:37:18 +0530

    Create 111t.c

---
+++ 111t.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void)#include <stdio.h>
+int main()
+{
+	int x=1,y=1,n,z,f;
+	printf("enter the number ");
+	scanf("%d",&n);
+	printf("\n %d \t %d",x,y);
+	for(f=1;f<=n-2;f++)
+	{
+		z=x+y;
+		printf("\t%d",z);
+		x=y;
+		y=z;
+		
+		
+	}
+printf("Biggest number %d",big);
+}
+{
+int x,y;
+pri
