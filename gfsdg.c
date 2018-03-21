1c6e9381dd29c0191c8b677d081114f797daedc4
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-21 16:14:17 +0530

    Create gfsdg.c

---
+++ gfsdg.c
@@ -0,0 +1,18 @@
+gf#include<stdio.h>
+int main()
+{
+    if(ain(void) {
+	int n;
+	scanf("%d\n",&n);
+	if(n%7==0)
+	{
+		printf("yes multiple of 7");
+	}
+	else
+	{
+		printf("not multiple of 7");
+	}
+	return 0;
+}
+© 2018 GitHub, Inc.
+    if 
