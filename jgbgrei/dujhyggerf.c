a48f23d6fe5d791cf3db8e469f1040226931e454
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-21 16:15:27 +0530

    Create dujhyggerf.c

---
+++ jgbgrei/dujhyggerf.c
@@ -0,0 +1,25 @@
+gf#include<stdio.h>
+int main()
+{
+    if(ain(void) {
+	int n;
+	scanf("%d\n",&bvfdhjhjghjgfhfgh
+  fhd
+  fd
+  hfdsnfbjkl
+  gbfnbkh
+  fdkbmvcbl
+  dfnkbv;
+  bfg,.n n);
+	if(n%7==0)
+	{
+		printf("yes multiple of 7");
+	}
+	else
+	{
+		printf("not multiple of 7");
+	}
+	return 0;
+}
+© 2018 GitHub, Inc.
+    if 
