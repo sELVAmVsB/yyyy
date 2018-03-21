7fee3595e634956efcf56207caaa9a215661a954
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-21 16:16:20 +0530

    Create ghhsgfhvfgsd.c

---
+++ ghhsgfhvfgsd.c
@@ -0,0 +1,31 @@
+gf#include<stdio.h>
+int main()
+{
+    if(ain(void) teo-rtgierwfgrfughwer
+    gnfdjghdfs
+    fjdkghf
+    gnfkgjd
+    gjkcvgjkfd
+    ghgbjdf
+    gmkfdh';f{
+	int n;
+	scanf("%d\n",&bvfdhjhjghjgfhfgh
+  fhd
+  fd
+  hfdsnfbjkl
+  gbfnbkh
+  fdkbmvcbl
+  dfnkbv;
+  bfg,.n n);
+	if(n%7==0)
+	{
+		printf("yes multiple of 7");
+	}
+	else
+	{
+		printf("not multiple of 7");
+	}
+	return 0;
+}
+© 2018 GitHub, Inc.
+    if 
