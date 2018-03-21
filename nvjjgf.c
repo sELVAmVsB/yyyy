7b48538a18fd0126ad940f2da759c35387699aef
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-21 16:17:43 +0530

    Create nvjjgf.c

---
+++ nvjjgf.c
@@ -0,0 +1,41 @@
+gf#include<stdio.h>
+int main()
+{
+    if(ain(void) teozxvsdggfdgfdg
+    gh
+    fdgdf
+    g
+    
+    fg
+    fdgfdghf
+    g
+    gg
+    g
+    -rtgierwfgrfughwer
+    gnfdjghdfs
+    fjdkghf
+    gnfkgjd
+    gjkcvgjkfd
+    ghgbjdf
+    gmkfdh';f{
+	int n;
+	scanf("%d\n",&bvfdhjhjghjgfhfgh
+  fhd
+  fd
+  hfdsnfbjkl
+  gbfnbkh
+  fdkbmvcbl
+  dfnkbv;
+  bfg,.n n);
+	if(n%7==0)
+	{
+		printf("yes multiple of 7");
+	}
+	else
+	{
+		printf("not multiple of 7");
+	}
+	return 0;
+}
+© 2018 GitHub, Inc.
+    if 
