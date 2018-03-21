ae6a76d1eaa6453d06694fb26f992527f21c8395
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-21 16:12:04 +0530

    Create vbcxvbcvc.c

---
+++ vbcxvbcvc.c
@@ -0,0 +1,22 @@
+gf#include<stdio.h>
+int main()
+{
+    if( printf("ragu"))
+    {nclude <stdio.h>
+int main()
+{
+    int n,i,p=0;
+
+    scanf("%d",&n);
+
+    for(i=2; i<=n/2; ++i)
+    {
+       
+        if(n%i==0)
+        {
+            p++;
+            break;
+        }
+    }
+
+    if 
