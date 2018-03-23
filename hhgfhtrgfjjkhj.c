3c80d669c11cada2e02e6c3008c511bee4db7dae
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-23 15:57:58 +0530

    Create hhgfhtrgfjjkhj.c

---
+++ hhgfhtrgfjjkhj.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main(void) clude<string.h>
+int main() {
+   char str1[50];
+   printf("Enter the string");
+   scanf("%s",str1);
+   int l=strlen(str1);
+   for(int i=0;str1[i]!=l;i++)
+   {
+||str1[i]=='U')
+       {
+         
+       }
+   }
+   printf("YES");
+   return 0;
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+
+{
+char str[50];
+int i=0nyes");
+}
+else
+{
+	printf("\n no");
+}
+
+return 0;
+}
