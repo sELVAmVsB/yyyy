84f02bda31c50d6447f341959b3d07b46244fba4
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-28 15:51:27 +0530

    Create jgfddfgfdf.c

---
+++ jgfddfgfdf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int x,y,diff;
+printf("enter number of team members in kabali's team");
+scanf("%d",&x);
+printf("enter number of team members in opponent team");
+scanf("%d",&y);
+diff=x-y;
+printf(include<stdio.h>
+void main()
+{
+float a,b,c;
+scanf("%f\t%f",&a,&b);
+c=a*b;
+printf("%f",c);
+}
