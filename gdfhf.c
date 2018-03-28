73b04e1d107b002638c2e5e77c1b43dae5589fb5
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-03-28 15:49:15 +0530

    Create gdfhf.c

---
+++ gdfhf.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void main()
+{
+int x,y,diff;
+printf("enter number of team members in kabali's team");
+scanf("%d",&x);
+printf("enter number of team members in opponent team");
+scanf("%d",&y);
+diff=x-y;
+printf("difference is %d\n",diff);
+if(x>y)
+{
+printe <stdio.h>
+int main (void)
+{
+
+int i,s=1,diff,j;                              
+int a[20];
+
+     while( s != 0 )                            
+    {
+for(i=0;i<2;i++)
+         scanf("%d", &a[i]);                   
+
+         if( a[i] != EOF )
+         {
+	for(i=0;i<2;i++)
+	{
+		for(j=i+1;j<2;j++)
+	{
+	diff=a[j]-a[i];
+	printf("%d\n",diff);
+	}
+	}
+         }	
+    }
+	return 0;
+}
+
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
